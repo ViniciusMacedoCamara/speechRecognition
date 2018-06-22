2b7e311e05ff503148aa9296d7abe49cdbcc437b
Vinicius(PersonalComputer) <vini.macedo.c@gmail.com>
2018-06-22 17:42:56 -0300

    mais ajuste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,7 @@ import { ChangeDetectorRef } from '@angular/core';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  //matches: String[];
-  msg:string;
+  matches: String[];
   isRecording = false;
  
   constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition, private plt: Platform, private cd: ChangeDetectorRef) { }
@@ -34,35 +33,12 @@ export class HomePage {
       });
   }
  
-  comandoDeVoz(){
-    this.msg = "";
-    this.speechRecognition.isRecognitionAvailable().then((available: boolean) => {
-      if(available){
-        this.speechRecognition.hasPermission().then((hasPermission: boolean) => {
-          if(hasPermission){
-            this.speechRecognition.startListening({language:'pt-BR'}).subscribe(
-                (matches: Array<string>) => {
-                  for(let item of matches){
-                    this.msg += item + ' ';
-                  }
-                },
-                (onerror) => alert('Erro: ' + onerror)
-              );
-          }else{
-            this.speechRecognition.requestPermission().then(
-                () => {
-                  this.comandoDeVoz();
-                },
-                () => {
-                  this.msg = "Você negou a permissão para comando de voz.";
-                }
-              )
-          }
-        })
-      }else{
-        this.msg = "Recurso de comando não está disponível.";
-      }
+  startListening() {
+    this.speechRecognition.startListening({language:'pt-BR'}).subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
     });
+    this.isRecording = true;
   }
  
 }
